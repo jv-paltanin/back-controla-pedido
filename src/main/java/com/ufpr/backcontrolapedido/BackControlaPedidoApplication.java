79f705d0b9f2ffa77ad2d99b207ae08a6097fe4b
Joao VP <joaovpaltanin116@gmail.com>
2022-08-04 10:40:37 -0300

    commit inicial

---
+++ src/main/java/com/ufpr/backcontrolapedido/BackControlaPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.backcontrolapedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackControlaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackControlaPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpr/backcontrolapedido/BackControlaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.backcontrolapedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackControlaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
