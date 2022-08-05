cb23bb1b46a335984dead7d2417698f76f247db2
Joao VP <joaovpaltanin116@gmail.com>
2022-08-05 00:56:19 -0300

    Métodos rest para objeto cliente

---
+++ src/main/java/com/ufpr/backcontrolapedido/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.ufpr.backcontrolapedido.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "cpf", nullable = false, length = 11)
+    private Long cpf;
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
+    @Column(name = "sobrenome", nullable = false, length = 70)
+    private String sobrenome;
+
+}
