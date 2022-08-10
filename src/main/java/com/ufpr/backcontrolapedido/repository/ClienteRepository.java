3f0117216f0277fcdacb50ae3b4cccd415700f4f
Joao VP <joaovpaltanin116@gmail.com>
2022-08-09 22:50:15 -0300

    refatorando padrão do projeto e retorno de exceções

--- src/main/java/com/ufpr/backcontrolapedido/repository/ClienteRepository.java
+++ src/main/java/com/ufpr/backcontrolapedido/repository/ClienteRepository.java
@@ -3,7 +3,7 @@ package com.ufpr.backcontrolapedido.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ufpr.backcontrolapedido.model.Cliente;
+import com.ufpr.backcontrolapedido.model.entities.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
---
+++ src/main/java/com/ufpr/backcontrolapedido/service/ClienteService.java
@@ -0,0 +1,74 @@
+package com.ufpr.backcontrolapedido.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ufpr.backcontrolapedido.model.dto.ClienteDTO;
+import com.ufpr.backcontrolapedido.model.entities.Cliente;
+import com.ufpr.backcontrolapedido.repository.ClienteRepository;
+import com.ufpr.backcontrolapedido.service.exception.ResourceNotFoundException;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    // recupera todos os clientes
+    public List<ClienteDTO> findAll() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes.stream().map(cliente -> new ClienteDTO(cliente)).collect(Collectors.toList());
+    }
+
+    // recupera cliente por id específico
+    public ClienteDTO findById(Long id) {
+        Cliente entity = getEntityById(id);
+        return new ClienteDTO(entity);
+    }
+
+    // insere cliente no banco de dados
+    public ClienteDTO insert(ClienteDTO dto) {
+        Cliente entity = new Cliente();
+        copyDtoToEntity(dto, entity);
+        entity = clienteRepository.save(entity);
+        return new ClienteDTO(entity);
+    }
+
+    // atualiza cliente no banco de dados
+    public ClienteDTO update(Long id, ClienteDTO dto) {
+        Cliente entity = getEntityById(id);
+        copyDtoToEntityForUpdate(dto, entity);
+        entity = clienteRepository.save(entity);
+        return new ClienteDTO(entity);
+    }
+
+    // deleta cliente por id
+    public void delete(Long id) {
+        getEntityById(id);
+        clienteRepository.deleteById(id);
+    }
+
+    // método encapsulado para recuperar entidade cliente por id do banco de dados
+    private Cliente getEntityById(Long id) {
+        Optional<Cliente> result = clienteRepository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado."));
+    }
+
+    // método encapsulado que transfere objeto DTO (Data transfer object) para a
+    // entidade que terá contato com banco de dados diretamente
+    private void copyDtoToEntity(ClienteDTO dto, Cliente entity) {
+        entity.setCpf(dto.getCpf());
+        entity.setNome(dto.getNome());
+        entity.setSobrenome(dto.getSobrenome());
+    }
+
+    private void copyDtoToEntityForUpdate(ClienteDTO dto, Cliente entity) {
+        entity.setNome(dto.getNome());
+        entity.setSobrenome(dto.getSobrenome());
+    }
+
+}
