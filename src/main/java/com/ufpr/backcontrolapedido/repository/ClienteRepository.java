4d960099217564bcec609770bf94a6d0aae5d127
João VP <joaovpaltanin116@gmail.com>
2022-12-18 18:45:52 -0300

    refactor: ajustando pom de anotações desnecessárias

--- src/main/java/com/ufpr/backcontrolapedido/repository/ClienteRepository.java
+++ src/main/java/com/ufpr/backcontrolapedido/repository/ClienteRepository.java
@@ -1,11 +1,9 @@
 package com.ufpr.backcontrolapedido.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ufpr.backcontrolapedido.model.entities.Cliente;
 
-@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     boolean existsClienteByCpf(Long cpf);
 }
